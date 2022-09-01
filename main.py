b9c08149cb05201b055863504c87507b0b346758
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-09-01 18:16:20 +0330

    Create Basic Hangman Function

--- main.py
+++ main.py
@@ -18,26 +18,26 @@ def get_valid_word(words):
         word = random.choice(words)
     return word.upper()
 
-# def hangman():
-#     word = get_valid_word(words)
-#     word_letters = set(word)
-#     alphabet = set(string.ascii_uppercase)
-#     used_letters = set()
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
     
-#     # getting user input
-#     user_letter = input('Guess a letter: ').upper()
-#     if user_letter in alphabet - used_letters:
-#         used_letters.add(user_letter)
-#         if user_letter in word_letters:
-#             word_letters.remove(user_letter)
+    # getting user input
+    user_letter = input('Guess a letter: ').upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
 
-#     elif user_letter in used_letters:
-#         print('You used this letter once, Please guess another letter.')
+    elif user_letter in used_letters:
+        print('You used this letter once, Please guess another letter.')
     
-#     else:
-#         print('Invalid charecter, Please try again.') 
+    else:
+        print('Invalid charecter, Please try again.') 
         
     
 
            
-# hangman()
+hangman()
